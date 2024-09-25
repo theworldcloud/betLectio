a3dc67cbaad791c0c5ae2e6eb5bb38c7064d44b1
Mathias Sørensen <mathiassrensen514@gmail.com>
2024-09-25 15:19:55 +0200

    done

--- dist/script.js
+++ dist/script.js
@@ -1,33 +1,53 @@
 "use strict";
-editUserText();
-createLogoutButton();
-function editUserText() {
+var header = {};
+console.log(header);
+header.editImageLink = function () {
+    var image = document.querySelector(".ls-master-header-logo");
+    var menu = document.querySelector("#s_m_ctl17") || document.querySelector("#m_ctl16");
+    if (!image || !menu)
+        return;
+    image.href = menu.href;
+};
+header.editUserText = function () {
     var container = document.querySelector("header .ls-master-header-institution");
     if (!container)
         return;
-    var newText = container.innerText.replace("&nbsp; Bruger:", "").trim();
-    var school = newText.replace("&nbsp;", "").trim();
-    container.textContent = "";
+    container.innerHTML = container.innerHTML.replace("&nbsp; Bruger:", "").trim();
+    var school = container.innerHTML.split("&nbsp;")[0].trim();
+    container.innerHTML = container.innerHTML.replace(school, "").trim();
+    container.innerHTML = container.innerHTML.replace("&nbsp;", "").trim();
     var profileText = document.querySelector("#s_m_HeaderContent_MainTitle .ls-hidden-smallscreen");
     var profileButton = document.querySelector("#s_m_HeaderContent_subnavigator_ctl12");
     var navbarProfile = document.querySelector("header .ls-master-header-institution .ls-user-name");
     if (!profileText || !profileButton || !navbarProfile)
-        return;
+        return profileText === null || profileText === void 0 ? void 0 : profileText.remove();
     var _a = profileText.innerHTML.split(", "), name = _a[0], grade = _a[1];
     grade = grade.replace("-", "").trim();
-    name = name.split(" ")[1];
+    var names = name.split(" ");
+    name = "".concat(names[1], " ").concat(names[names.length - 1]);
     navbarProfile.text = "".concat(name, ", ").concat(grade, " ").concat(school);
     navbarProfile.href = profileButton.href;
     var element = document.createElement("span");
     container.append(element);
-}
-function createLogoutButton() {
+};
+header.createLogoutButton = function () {
     var container = document.querySelector("header .ls-master-header-institution");
-    var logoutButton = document.querySelector("#s_m_ctl18");
+    var logoutButton = document.querySelector("#s_m_ctl18 ");
     if (!container || !logoutButton)
         return;
     var element = document.createElement("a");
     element.href = logoutButton.href;
     element.text = "Log ud";
     container.appendChild(element);
-}
+};
+header.createLoginButton = function () {
+    var container = document.querySelector("header .ls-master-header-institution");
+    var logoutButton = document.querySelector("#m_ctl17 ");
+    if (!container || !logoutButton)
+        return;
+    var element = document.createElement("a");
+    element.href = logoutButton.href;
+    element.text = "Log ind";
+    container.appendChild(element);
+};
+Object.keys(header).forEach(function (func) { return header[func](); });
--- src/scripts/header.ts
+++ src/scripts/header.ts
@@ -1,21 +1,32 @@
-editUserText();
-createLogoutButton();
+const header: Record<string,() => void> = {};
+console.log(header);
 
-function editUserText() {
+header.editImageLink = function() {
+    const image: HTMLAnchorElement | null = document.querySelector(".ls-master-header-logo");
+    const menu: HTMLAnchorElement | null = document.querySelector("#s_m_ctl17") || document.querySelector("#m_ctl16");
+    if (!image || !menu) return;
+
+    image.href = menu.href;
+}
+
+header.editUserText = function() {
     const container: HTMLDivElement | null = document.querySelector("header .ls-master-header-institution");
     if (!container) return;
 
     container.innerHTML = container.innerHTML.replace("&nbsp; Bruger:", "").trim();
-    const school = container.innerText.replace("&nbsp;", ``).trim();
+    const school = container.innerHTML.split("&nbsp;")[0].trim();
+    container.innerHTML = container.innerHTML.replace(school, "").trim();
+    container.innerHTML = container.innerHTML.replace(`&nbsp;`, "").trim();
 
     const profileText = document.querySelector("#s_m_HeaderContent_MainTitle .ls-hidden-smallscreen");
     const profileButton: HTMLAnchorElement | null = document.querySelector("#s_m_HeaderContent_subnavigator_ctl12");
     const navbarProfile: HTMLAnchorElement | null = document.querySelector("header .ls-master-header-institution .ls-user-name");
-    if (!profileText || !profileButton || !navbarProfile) return;
+    if (!profileText || !profileButton || !navbarProfile) return profileText?.remove();
 
     let [ name, grade ] = profileText.innerHTML.split(", ");
     grade = grade.replace("-", "").trim();
-    name = name.split(" ")[1];
+    const names = name.split(" ");
+    name = `${names[1]} ${names[names.length - 1]}`;
 
     navbarProfile.text = `${name}, ${grade} ${school}`;
     navbarProfile.href = profileButton.href;
@@ -24,9 +35,9 @@ function editUserText() {
     container.append(element);
 }
 
-function createLogoutButton() {
+header.createLogoutButton = function() {
     const container = document.querySelector("header .ls-master-header-institution");
-    const logoutButton: HTMLAnchorElement | null = document.querySelector("#s_m_ctl18");
+    const logoutButton: HTMLAnchorElement | null = document.querySelector("#s_m_ctl18 ");
     if (!container || !logoutButton) return;
 
     const element = document.createElement("a");
@@ -35,3 +46,18 @@ function createLogoutButton() {
 
     container.appendChild(element);
 }
+
+header.createLoginButton = function() {
+    const container = document.querySelector("header .ls-master-header-institution");
+    const logoutButton: HTMLAnchorElement | null = document.querySelector("#m_ctl17 ");
+    if (!container || !logoutButton) return;
+
+    const element = document.createElement("a");
+    element.href = logoutButton.href;
+    element.text = "Log ind";
+
+    container.appendChild(element);
+}
+
+
+Object.keys(header).forEach(func => header[func]());
