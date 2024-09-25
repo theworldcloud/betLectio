3436a44eaf8c1a40d11a5a641329a2f1de1903a3
theworldcloud <mathiassrensen514@gmail.com>
2024-09-26 00:44:11 +0200

    done

--- dist/script.js
+++ dist/script.js
@@ -1,9 +1,8 @@
 "use strict";
 var header = {};
-console.log(header);
 header.editImageLink = function () {
     var image = document.querySelector(".ls-master-header-logo");
-    var menu = document.querySelector("#s_m_ctl17") || document.querySelector("#m_ctl16");
+    var menu = document.querySelector("#s_m_ctl17") || document.querySelector("#m_ctl17");
     if (!image || !menu)
         return;
     image.href = menu.href;
@@ -19,20 +18,32 @@ header.editUserText = function () {
     var profileText = document.querySelector("#s_m_HeaderContent_MainTitle .ls-hidden-smallscreen");
     var profileButton = document.querySelector("#s_m_HeaderContent_subnavigator_ctl12");
     var navbarProfile = document.querySelector("header .ls-master-header-institution .ls-user-name");
-    if (!profileText || !profileButton || !navbarProfile)
-        return profileText === null || profileText === void 0 ? void 0 : profileText.remove();
+    if (!profileText || !profileButton || !navbarProfile) {
+        if (!navbarProfile)
+            return;
+        var username = navbarProfile.text;
+        navbarProfile.text = "".concat(school, " (").concat(username, ")");
+        navbarProfile.href = "";
+        navbarProfile.style.pointerEvents = "none";
+        var element_1 = document.createElement("span");
+        container.append(element_1);
+        return;
+    }
     var _a = profileText.innerHTML.split(", "), name = _a[0], grade = _a[1];
     grade = grade.replace("-", "").trim();
     var names = name.split(" ");
     name = "".concat(names[1], " ").concat(names[names.length - 1]);
     navbarProfile.text = "".concat(name, ", ").concat(grade, " ").concat(school);
     navbarProfile.href = profileButton.href;
+    if (navbarProfile.href === window.location.pathname) {
+        navbarProfile.classList.add("nav-active");
+    }
     var element = document.createElement("span");
     container.append(element);
 };
 header.createLogoutButton = function () {
     var container = document.querySelector("header .ls-master-header-institution");
-    var logoutButton = document.querySelector("#s_m_ctl18 ");
+    var logoutButton = document.querySelector("#s_m_ctl18") || document.querySelector("#m_ctl18");
     if (!container || !logoutButton)
         return;
     var element = document.createElement("a");
@@ -42,6 +53,9 @@ header.createLogoutButton = function () {
 };
 header.createLoginButton = function () {
     var container = document.querySelector("header .ls-master-header-institution");
+    var userText = document.querySelector("header .ls-master-header-institution .ls-user-name");
+    if (userText)
+        return;
     var logoutButton = document.querySelector("#m_ctl17 ");
     if (!container || !logoutButton)
         return;
@@ -49,5 +63,79 @@ header.createLoginButton = function () {
     element.href = logoutButton.href;
     element.text = "Log ind";
     container.appendChild(element);
+};
+header.createNavLinks = function () {
+    function _createLink(element) {
+        var link = document.createElement("a");
+        link.href = element.href;
+        link.text = element.text;
+        if (element.id.includes("subnavigator_ctl04"))
+            link.href = element.href.replace("fravaerelev_fravaersaarsager", "fravaerelev");
+        var shouldSlice = false;
+        var sliceLinks = ["Karakterer", "Studieplan", "Bøger", "Dokumenter"];
+        sliceLinks.forEach(function (text) {
+            if (shouldSlice)
+                return;
+            if (element.text === text)
+                shouldSlice = true;
+        });
+        if (shouldSlice)
+            link.href = element.href.slice(0, link.href.indexOf("?"));
+        var windowHref = window.location.origin + window.location.pathname;
+        if (link.href === windowHref) {
+            link.classList.add("nav-active");
+        }
+        return link;
+    }
+    var container = document.querySelector("header nav");
+    if (!container)
+        return;
+    var links = [];
+    var hiddenLinks = [];
+    var home = document.querySelector("header nav #s_m_ctl16") || document.querySelector("header nav #m_ctl16");
+    if (home)
+        links.push(_createLink(home));
+    var elements = document.querySelectorAll(".ls-subnav-container .ls-subnav1 div a");
+    elements.forEach(function (element, index) {
+        if (element.text == "Skema" && links.length === 1)
+            return links.push(_createLink(element));
+        if (element.text == "Fravær" && links.length === 2)
+            return links.push(_createLink(element));
+        if (element.text == "Opgaver" && links.length === 3)
+            return links.push(_createLink(element));
+        if (element.text == "Lektier" && links.length === 4)
+            return links.push(_createLink(element));
+        if (element.text == "Karakterer" && links.length === 5)
+            return links.push(_createLink(element));
+        if (element.text == "Beskeder" && links.length === 6)
+            return links.push(_createLink(element));
+        if (element.text == "Studieplan" && hiddenLinks.length === 0)
+            return hiddenLinks.push(_createLink(element));
+        if (element.text == "Bøger" && hiddenLinks.length === 1)
+            return hiddenLinks.push(_createLink(element));
+        if (element.text == "Dokumenter" && hiddenLinks.length === 2)
+            return hiddenLinks.push(_createLink(element));
+        return;
+    });
+    container.append.apply(container, links);
+    if (elements.length < 2)
+        return;
+    var spanElement = document.createElement("span");
+    container.append(spanElement);
+    var hiddenLinkInput = document.createElement("input");
+    hiddenLinkInput.type = "checkbox";
+    hiddenLinkInput.id = "nav-toggle-hidden-links";
+    var hiddenLinkActive = false;
+    var hiddenLinkLabel = document.createElement("label");
+    hiddenLinkLabel.htmlFor = "nav-toggle-hidden-links";
+    hiddenLinks.forEach(function (link) {
+        hiddenLinkLabel.append(link);
+        if (link.classList.contains("nav-active")) {
+            hiddenLinkActive = true;
+        }
+    });
+    hiddenLinkInput.checked = hiddenLinkActive;
+    container.append(hiddenLinkInput);
+    container.append(hiddenLinkLabel);
 };
 Object.keys(header).forEach(function (func) { return header[func](); });
--- src/scripts/header.ts
+++ src/scripts/header.ts
@@ -1,9 +1,8 @@
 const header: Record<string,() => void> = {};
-console.log(header);
 
 header.editImageLink = function() {
     const image: HTMLAnchorElement | null = document.querySelector(".ls-master-header-logo");
-    const menu: HTMLAnchorElement | null = document.querySelector("#s_m_ctl17") || document.querySelector("#m_ctl16");
+    const menu: HTMLAnchorElement | null = document.querySelector("#s_m_ctl17") || document.querySelector("#m_ctl17");
     if (!image || !menu) return;
 
     image.href = menu.href;
@@ -21,7 +20,19 @@ header.editUserText = function() {
     const profileText = document.querySelector("#s_m_HeaderContent_MainTitle .ls-hidden-smallscreen");
     const profileButton: HTMLAnchorElement | null = document.querySelector("#s_m_HeaderContent_subnavigator_ctl12");
     const navbarProfile: HTMLAnchorElement | null = document.querySelector("header .ls-master-header-institution .ls-user-name");
-    if (!profileText || !profileButton || !navbarProfile) return profileText?.remove();
+    if (!profileText || !profileButton || !navbarProfile) {
+        if (!navbarProfile) return;
+        const username = navbarProfile.text;
+
+        navbarProfile.text = `${school} (${username})`;
+        navbarProfile.href = "";
+        navbarProfile.style.pointerEvents = "none";
+
+        const element = document.createElement("span");
+        container.append(element);
+
+        return;
+    }
 
     let [ name, grade ] = profileText.innerHTML.split(", ");
     grade = grade.replace("-", "").trim();
@@ -31,13 +42,17 @@ header.editUserText = function() {
     navbarProfile.text = `${name}, ${grade} ${school}`;
     navbarProfile.href = profileButton.href;
 
+    if (navbarProfile.href === window.location.pathname) {
+        navbarProfile.classList.add("nav-active");
+    }
+
     const element = document.createElement("span");
     container.append(element);
 }
 
 header.createLogoutButton = function() {
     const container = document.querySelector("header .ls-master-header-institution");
-    const logoutButton: HTMLAnchorElement | null = document.querySelector("#s_m_ctl18 ");
+    const logoutButton: HTMLAnchorElement | null = document.querySelector("#s_m_ctl18") || document.querySelector("#m_ctl18");
     if (!container || !logoutButton) return;
 
     const element = document.createElement("a");
@@ -49,6 +64,9 @@ header.createLogoutButton = function() {
 
 header.createLoginButton = function() {
     const container = document.querySelector("header .ls-master-header-institution");
+    const userText = document.querySelector("header .ls-master-header-institution .ls-user-name");
+    if (userText) return;
+
     const logoutButton: HTMLAnchorElement | null = document.querySelector("#m_ctl17 ");
     if (!container || !logoutButton) return;
 
@@ -59,5 +77,82 @@ header.createLoginButton = function() {
     container.appendChild(element);
 }
 
+header.createNavLinks = function() {
+    function _createLink(element: HTMLAnchorElement) {
+        const link = document.createElement("a");
+
+        link.href = element.href;
+        link.text = element.text;
+
+        if (element.id.includes("subnavigator_ctl04")) link.href = element.href.replace("fravaerelev_fravaersaarsager", "fravaerelev");
+
+        let shouldSlice= false;
+        const sliceLinks = [ "Karakterer", "Studieplan", "Bøger", "Dokumenter" ];
+        sliceLinks.forEach(function(text) {
+            if (shouldSlice) return;
+            if (element.text === text) shouldSlice = true;
+        });
+
+        if (shouldSlice) link.href = element.href.slice(0, link.href.indexOf("?"));
+
+        const windowHref = window.location.origin + window.location.pathname;
+        if (link.href === windowHref) {
+            link.classList.add("nav-active");
+        }
+
+        return link;
+    }
+
+    const container = document.querySelector("header nav");
+    if (!container) return;
+
+    const links: Array<HTMLAnchorElement> = [];
+    const hiddenLinks: Array<HTMLAnchorElement> = [];
+
+    const home: HTMLAnchorElement | null = document.querySelector("header nav #s_m_ctl16") || document.querySelector("header nav #m_ctl16");
+    if (home) links.push(_createLink(home));
+
+    const elements: NodeListOf<HTMLAnchorElement> = document.querySelectorAll(".ls-subnav-container .ls-subnav1 div a");
+    elements.forEach(function(element, index) {
+        if (element.text == "Skema" && links.length === 1) return links.push(_createLink(element));
+        if (element.text == "Fravær" && links.length === 2) return links.push(_createLink(element));
+        if (element.text == "Opgaver" && links.length === 3) return links.push(_createLink(element));
+        if (element.text == "Lektier" && links.length === 4) return links.push(_createLink(element));
+        if (element.text == "Karakterer" && links.length === 5) return links.push(_createLink(element));
+        if (element.text == "Beskeder" && links.length === 6) return links.push(_createLink(element));
+
+        if (element.text == "Studieplan" && hiddenLinks.length === 0) return hiddenLinks.push(_createLink(element));
+        if (element.text == "Bøger" && hiddenLinks.length === 1) return hiddenLinks.push(_createLink(element));
+        if (element.text == "Dokumenter" && hiddenLinks.length === 2) return hiddenLinks.push(_createLink(element));
+
+        return;
+    });
+
+    container.append(...links);
+    if (elements.length < 2) return;
+
+    const spanElement = document.createElement("span");
+    container.append(spanElement);
+
+    const hiddenLinkInput = document.createElement("input");
+    hiddenLinkInput.type = "checkbox";
+    hiddenLinkInput.id = "nav-toggle-hidden-links";
+
+    let hiddenLinkActive = false;
+    const hiddenLinkLabel = document.createElement("label");
+    hiddenLinkLabel.htmlFor = "nav-toggle-hidden-links";
+    hiddenLinks.forEach(function(link) {
+        hiddenLinkLabel.append(link);
+
+        if (link.classList.contains("nav-active")) {
+            hiddenLinkActive = true;
+        }
+    });
+
+    hiddenLinkInput.checked = hiddenLinkActive;
+
+    container.append(hiddenLinkInput);
+    container.append(hiddenLinkLabel);
+}
 
 Object.keys(header).forEach(func => header[func]());
